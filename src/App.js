af60fed8273f7a9e1786d90e656decf701b2e81e
abrarasif11 <faasif929@gmail.com>
2022-11-23 22:36:05 +0600

    router-setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layout/Main';
+import Home from './Pages/Home/Home';
+import Blog from './Pages/Blog/Blog';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
-    
+    const router = createBrowserRouter([
+      {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+          {
+            path: '/',
+            element: <Home></Home>
+          },
+          {
+            path: '/blog',
+            element : <Blog></Blog>
+          },
+          {
+            path: '/login',
+            element: <Login></Login>
+          },
+          {
+            path: '/signup',
+            element : <SignUp></SignUp>
+          }
+        ]
+      }
+    ])
   return (
     <div className="App">
-       
+     <RouterProvider router={router}></RouterProvider>  
     </div>
   );
 }
