53da3d73821bf79897912c3599a201f7d1e19ae0
abrarasif11 <faasif929@gmail.com>
2022-11-24 02:28:05 +0600

    sign-up

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -7,12 +7,12 @@ const Login = () => {
             <h1 className="text-2xl font-bold text-center">Login</h1>
             <form novalidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
                 <div className="space-y-1 text-sm">
-                    <label for="username" className="block dark:text-gray-400">Username</label>
-                    <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                    <label for="email" className="block dark:text-gray-400">Email</label>
+                    <input type="email" name="username" id="email" placeholder="email" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
                 </div>
                 <div className="space-y-1 text-sm">
                     <label for="password" className="block dark:text-gray-400">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                    <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
                     <div className="flex justify-end text-xs dark:text-gray-400">
                         <a rel="noopener noreferrer" href="#">Forgot Password?</a>
                     </div>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,33 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-900 dark:text-gray-100">
-	<h1 className="text-2xl font-bold text-center">Login</h1>
-	<form novalidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
-		<div className="space-y-1 text-sm">
-			<label for="username" className="block dark:text-gray-400">Username</label>
-			<input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
-		</div>
-		<div className="space-y-1 text-sm">
-			<label for="password" className="block dark:text-gray-400">Password</label>
-			<input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
-			<div className="flex justify-end text-xs dark:text-gray-400">
-				<a rel="noopener noreferrer" href="#">Forgot Password?</a>
-			</div>
-		</div>
-		<button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400">Sign in</button>
-	</form>
-	<div className="flex items-center pt-4 space-x-1">
-		<div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-		<p className="px-3 text-sm dark:text-gray-400">Login with social accounts</p>
-		<div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-	</div>
-	
-	<p className="text-xs text-center sm:px-6 dark:text-gray-400">Don't have an account?
-		<a rel="noopener noreferrer" href="#" className="underline dark:text-gray-100">Sign up</a>
-	</p>
-</div>
+        <div className="w-full mx-auto mt-20 mb-20 max-w-md p-8 space-y-3 rounded-xl bg-gray-600 text-gray-100">
+        <h1 className="text-2xl font-bold text-center">Sign Up</h1>
+        <form  action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+            <div className="space-y-1 text-sm">
+                <label for="username" className="block dark:text-gray-400">Username</label>
+                <input type="text" name="username" id="username" placeholder="username" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
+            </div>
+            <div className="space-y-1 text-sm">
+                <label for="username" className="block dark:text-gray-400">Email</label>
+                <input type="email" name="email" id="username" placeholder="email" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
+            </div>
+            <div className="space-y-1 text-sm">
+                <label for="password" className="block dark:text-gray-400">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 text-black focus:dark:border-violet-400" />
+            </div>
+            <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 bg-orange-600">Sign Up</button>
+        </form>
+        <div className="flex justify-center space-x-4">
+              
+        </div>
+        <p className="text-xs text-center sm:px-6 dark:text-gray-400">Already have an account?
+            <Link  to="/login" className="underline text-xs dark:text-gray-100"> Log in</Link>
+        </p>
+    </div>
     );
 };
 
